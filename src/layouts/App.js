bc2065f50aeb85df2c73e5efe1803381b8b8881c
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-11-30 10:54:25 +0100

    init commit

---
+++ src/layouts/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return ( <
+    div className = "App" >
+    <
+    div > hello it 's me</div> <
+    /div>
+  );
+}
+
+export default App;
