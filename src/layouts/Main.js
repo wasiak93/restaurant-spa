6ae2209fd9daadf8e556be4c9d46913682835e23
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-12-02 12:26:21 +0100

    in Main.js add Switch and component ErrorPage

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import About from "../pages/About";
 import Menu from "../pages/Menu";
 
 const Contact = () => <h2> kontakt</h2>;
 const Prom = () => <h2> promocje</h2>;
+const ErrorPage = () => <h2>podana strona nie istnieje</h2>;
 
 const Main = () => {
   return (
     <div>
-      <Route path="/" exact component={About} />
-      <Route path="/menu" component={Menu} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/prom" component={Prom} />
+      <Switch>
+        <Route path="/" exact component={About} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/prom" component={Prom} />
+        <Route component={ErrorPage} />
+      </Switch>
     </div>
   );
 };
