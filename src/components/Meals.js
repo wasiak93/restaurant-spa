c4d5c744918bb0e7e3eaf3de497591423486912e
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-12-03 12:59:34 +0100

    add lunch and cart to the mealList in Meals.js

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -21,14 +21,56 @@ const mealsList = {
           "jajko sadzone, bekon, fasolka w sosie pomidorowym, 2 kiełbaski,tost, herbata"
       }
     ]
+  },
+  lunch: {
+    title: "Lunch",
+    details: [
+      {
+        id: 0,
+        name: "Danie dnia- Ślązak",
+        price: 19.9,
+        ingridents:
+          "Zupa pomidorowa, zrazy wołowe z czerwona kapustą i kluseczkami, kompot"
+      },
+      {
+        id: 1,
+        name: "Danie dnia- Fit",
+        price: 19.9,
+        ingridents:
+          "Zupa krem z brokułów, grilowana pierś z kurczaka z ryże brązowym i warzywami gotowanymi na parza, woda"
+      }
+    ]
+  },
+  cart: {
+    title: "Karta główna",
+    details: [
+      {
+        id: 0,
+        name: "Kotlet schabowy",
+        price: 26.9,
+        ingridents: "Kotlet schabowy z kością, ziemniaki z wody, mizeria"
+      },
+      {
+        id: 1,
+        name: "Placek po zbójnicku",
+        price: 29.9,
+        ingridents:
+          "2 placki ziemniaczane, mięso wołowe w sosie myśliwskim z papryką, pieczarkami, ogórkiem kiszony, wszystko posypane żółtym serem i okraszone kwaśną śmietaną"
+      },
+      {
+        id: 2,
+        name: "Zapiekanka po polsku",
+        price: 20.9,
+        ingridents:
+          "ziemniaki pieczone, mięso wołowe, kurczak, boczek, papryka, pomidor, cukinia, sos beszamelowy, wszystko przełożone plastrami żółtego sera i zapieczone"
+      }
+    ]
   }
 };
 const Meals = data => {
   const meal = data.match.params.id;
   const list = [...mealsList[meal].details];
-  console.log(meal);
-  console.log(list);
-  console.log(mealsList.meal);
+
   const meals = list.map(item => (
     <div key={item.id}>
       <div className="header_meal">
@@ -40,7 +82,7 @@ const Meals = data => {
   ));
   return (
     <div className="meal">
-      <h1>{meal.title}</h1>
+      <h1>{mealsList[meal].title}</h1>
       <div className="text">
         <MenuList />
         <section className="content">{meals}</section>
