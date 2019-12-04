befd893ee0c46aa12a8328d3d599c9c3278f0530
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-12-04 11:50:31 +0100

    change name style Meals.css instead of Breakfast.css

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,50 +1,43 @@
 import React from "react";
 import MenuList from "./MenuList";
-import "../styles/Breakfast.css";
+import "../styles/Meals.css";
 
 const mealsList = {
   breakfast: {
     title: "Śniadanie",
-    details: [
-      {
+    details: [{
         id: 0,
         name: "Jajecznica",
         price: 16.9,
-        ingridents:
-          "3 jajka, szczypiorek, boczek, masło, pomidor, ogórek, kromka chleba,herbata"
+        ingridents: "3 jajka, szczypiorek, boczek, masło, pomidor, ogórek, kromka chleba,herbata"
       },
       {
         id: 1,
         name: "Angielskie Śniadanie",
         price: 19.9,
-        ingridents:
-          "jajko sadzone, bekon, fasolka w sosie pomidorowym, 2 kiełbaski,tost, herbata"
+        ingridents: "jajko sadzone, bekon, fasolka w sosie pomidorowym, 2 kiełbaski,tost, herbata"
       }
     ]
   },
   lunch: {
     title: "Lunch",
-    details: [
-      {
+    details: [{
         id: 0,
         name: "Danie dnia- Ślązak",
         price: 19.9,
-        ingridents:
-          "Zupa pomidorowa, zrazy wołowe z czerwona kapustą i kluseczkami, kompot"
+        ingridents: "Zupa pomidorowa, zrazy wołowe z czerwona kapustą i kluseczkami, kompot"
       },
       {
         id: 1,
         name: "Danie dnia- Fit",
         price: 19.9,
-        ingridents:
-          "Zupa krem z brokułów, grilowana pierś z kurczaka z ryże brązowym i warzywami gotowanymi na parza, woda"
+        ingridents: "Zupa krem z brokułów, grilowana pierś z kurczaka z ryże brązowym i warzywami gotowanymi na parza, woda"
       }
     ]
   },
   cart: {
     title: "Karta główna",
-    details: [
-      {
+    details: [{
         id: 0,
         name: "Kotlet schabowy",
         price: 26.9,
@@ -54,15 +47,13 @@ const mealsList = {
         id: 1,
         name: "Placek po zbójnicku",
         price: 29.9,
-        ingridents:
-          "2 placki ziemniaczane, mięso wołowe w sosie myśliwskim z papryką, pieczarkami, ogórkiem kiszony, wszystko posypane żółtym serem i okraszone kwaśną śmietaną"
+        ingridents: "2 placki ziemniaczane, mięso wołowe w sosie myśliwskim z papryką, pieczarkami, ogórkiem kiszony, wszystko posypane żółtym serem i okraszone kwaśną śmietaną"
       },
       {
         id: 2,
         name: "Zapiekanka po polsku",
         price: 20.9,
-        ingridents:
-          "ziemniaki pieczone, mięso wołowe, kurczak, boczek, papryka, pomidor, cukinia, sos beszamelowy, wszystko przełożone plastrami żółtego sera i zapieczone"
+        ingridents: "ziemniaki pieczone, mięso wołowe, kurczak, boczek, papryka, pomidor, cukinia, sos beszamelowy, wszystko przełożone plastrami żółtego sera i zapieczone"
       }
     ]
   }
@@ -71,23 +62,40 @@ const Meals = data => {
   const meal = data.match.params.id;
   const list = [...mealsList[meal].details];
 
-  const meals = list.map(item => (
-    <div key={item.id}>
-      <div className="header_meal">
-        <h2>{item.name}</h2>
-        <p className="price">{item.price}</p>
-      </div>
-      <p>{item.ingridents}</p>
-    </div>
+  const meals = list.map(item => ( <
+    div key = {
+      item.id
+    } >
+    <
+    div className = "header_meal" >
+    <
+    h2 > {
+      item.name
+    } < /h2> <
+    p className = "price" > {
+      item.price
+    } < /p> <
+    /div> <
+    p > {
+      item.ingridents
+    } < /p> <
+    /div>
   ));
-  return (
-    <div className="meal">
-      <h1>{mealsList[meal].title}</h1>
-      <div className="text">
-        <MenuList />
-        <section className="content">{meals}</section>
-      </div>
-    </div>
+  return ( <
+    div className = "meal" >
+    <
+    h1 > {
+      mealsList[meal].title
+    } < /h1> <
+    div className = "text" >
+    <
+    MenuList / >
+    <
+    section className = "content" > {
+      meals
+    } < /section> <
+    /div> <
+    /div>
   );
 };
 
