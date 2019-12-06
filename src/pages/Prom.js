97fa8dfe25e3545d65d9ef9e60e26896197a0ec2
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-12-06 13:52:33 +0100

    add clas to h2 in Prom.js; style Prom.css

--- src/pages/Prom.js
+++ src/pages/Prom.js
@@ -2,18 +2,23 @@ import React from "react";
 import "../styles/Prom.css";
 
 const proms = [
-  { day: "Poniedziałek", discount: "Do każdego dania głównego kompot gratis" },
   {
+    position: "first",
+    day: "Poniedziałek",
+    discount: "Do każdego dania głównego kompot gratis"
+  },
+  {
+    position: "second",
     day: "Wtorek",
     discount: "Dla rachunków wyższych niż 200 złotych 10% zniżki"
   },
-  { day: "Czartek", discount: "Lunch za połowę ceny!" }
+  { position: "third", day: "Czartek", discount: "Lunch za połowę ceny!" }
 ];
 
 const Prom = () => {
   const promList = proms.map(item => (
     <div key={item.day}>
-      <h2>W każdy: {item.day}</h2>
+      <h2 className={item.position}>W każdy: {item.day}</h2>
       <p>{item.discount}</p>
     </div>
   ));
