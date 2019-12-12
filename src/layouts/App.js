2bf1e138e66673a3a07a631db0f8cee8da02872c
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-12-12 14:58:01 +0100

    update needed to git hub pages; add basename in Router- App.js

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -8,7 +8,7 @@ import Footer from "./Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <aside className="navigation">{<Navigation />}</aside>
         <header>
