0385c0e41ebe38876e6d844afdac3b31648aa1f2
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-12-12 14:44:56 +0100

    Updates

---
+++ precache-manifest.268550ca2bf8756602ee600e116ab982.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbfaf5d06786aa849fdfb2f8806b9e5",
+    "url": "/restaurant-spa/index.html"
+  },
+  {
+    "revision": "59905d8526b9ace8c2c9",
+    "url": "/restaurant-spa/static/css/main.7cc57267.chunk.css"
+  },
+  {
+    "revision": "4a9be8e4a87f55cc634b",
+    "url": "/restaurant-spa/static/js/2.03e147ee.chunk.js"
+  },
+  {
+    "revision": "59905d8526b9ace8c2c9",
+    "url": "/restaurant-spa/static/js/main.b78113d6.chunk.js"
+  },
+  {
+    "revision": "2340ad810386ba360b9c",
+    "url": "/restaurant-spa/static/js/runtime-main.f7799aeb.js"
+  },
+  {
+    "revision": "dc3de16936486aae57a7535c584b51db",
+    "url": "/restaurant-spa/static/media/restaurant1.dc3de169.jpg"
+  },
+  {
+    "revision": "054639064a3ebb179083faffa7894546",
+    "url": "/restaurant-spa/static/media/restaurant2.05463906.jpg"
+  },
+  {
+    "revision": "79d551714ab709fb52d6c58a9ef0bf62",
+    "url": "/restaurant-spa/static/media/restaurant3.79d55171.jpg"
+  },
+  {
+    "revision": "ccb29f9c7f5d47d51c5e9b18e9f5e11e",
+    "url": "/restaurant-spa/static/media/restaurant4.ccb29f9c.jpg"
+  }
+]);
